5cce12748b68f05b89a5b3d503f9e05c1335c54c
BPplays <BruceWplays@gmail.com>
2023-02-18 09:50:37 -0800

    fixed trying to delete and run wrong args

--- updater.lua
+++ updater.lua
@@ -4,18 +4,19 @@ if fs.exists("AndysPrograms/api/git/git") == false then
     fs.makeDir("AndysPrograms/api/git")
     shell.run("cd","AndysPrograms/api/git")
     shell.run("pastebin","get","WPg3HSiU","git")
-    shell.run("cd ","//")
+    shell.run("cd","//")
+    shell.run("cd","..")
 end
 if fs.exists("AndysPrograms/api/git/git") == false then
     fs.makeDir("AndysPrograms/api/git")
     shell.run("cd","AndysPrograms/api/git")
     shell.run("git","get","BPplays","CC-git-api","main","git","git")
-    shell.run("cd ","//")
+    shell.run("cd","//")
 end
 os.loadAPI("AndysPrograms/api/git/git")
 
 args[6] = shell.dir()
-shell.run("delete", arg[2])
+shell.run("delete", arg[5])
 -- shell.run("pastebin"," get ",arg[1], arg[2])
 git.get(args)
-shell.run(arg[2])
+shell.run(arg[5])
